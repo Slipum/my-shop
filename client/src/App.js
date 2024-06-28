b148f499dd68f52366574b6f9689b788bc2e6edb
Slipum <123569528+Slipum@users.noreply.github.com>
2024-06-28 16:13:13 +0300

    add Navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { getUserProfile } from './api';
 import Admin from './components/pages/Admin/Admin';
 import Cart from './components/pages/Cart/Cart';
 import Home from './components/pages/Home/Home';
@@ -9,16 +10,49 @@ import Profile from './components/pages/Profile/Profile';
 import Register from './components/pages/Register/Register';
 
 const App = () => {
+	const [userProfile, setUserProfile] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUserProfile = async () => {
+			try {
+				const response = await getUserProfile();
+				setUserProfile(response.data);
+			} catch (error) {
+				console.error('Error fetching profile:', error);
+				setUserProfile(null); // Установить профиль в null при ошибке
+			} finally {
+				setLoading(false); // Установить loading в false после завершения загрузки
+			}
+		};
+
+		fetchUserProfile();
+	}, []);
+
+	if (loading) {
+		return <div>Loading...</div>; // Можно отображать спиннер загрузки или другой индикатор
+	}
+
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
-				<Route path="/admin" element={<Admin />} />
-				<Route path="/products" element={<ProductList />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/profile" element={<Profile />} />
+				{userProfile && ( // Проверяем, что userProfile загружен
+					<>
+						<Route
+							path="/admin"
+							element={userProfile.username === 'admin' ? <Admin /> : <Navigate to="/" />}
+						/>
+						<Route path="/products" element={<ProductList />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="/profile" element={<Profile />} />
+					</>
+				)}
+				{!userProfile && ( // Если userProfile не загружен, перенаправляем на страницу логина
+					<Route path="/*" element={<Navigate to="/login" />} />
+				)}
 			</Routes>
 		</Router>
 	);
